2937b3b185d76601659415d4d99a710035143adb
Pierre Wieser <pwieser@trychlos.org>
2023-06-20 12:53:09 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:env-settings',
-    version: '1.4.0',
+    version: '1.4.1-rc',
     summary: 'Autoload settings (YAML, JSON) for server and client from private assets based on NODE_ENV.',
     git: 'https://github.com/trychlos/pwix-env-settings',
     documentation: 'README.md'
