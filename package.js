224fad1a1946c75d596b973f41433258aa9a6608
Pierre Wieser <p.wieser@trychlos.org>
2024-05-23 20:09:25 +0200

    Keep the summary under 100 chars

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'pwix:env-settings',
     version: '1.6.0-rc',
-    summary: 'Autoload settings (YAML, JSON) from (server-only) private assets based on APP_ENV (defaulting to NODE_ENV).',
+    summary: 'Autoload settings (YAML, JSON) from (server-only) private assets based on running environment.',
     git: 'https://github.com/trychlos/pwix-env-settings',
     documentation: 'README.md'
 });
